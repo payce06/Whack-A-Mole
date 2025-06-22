ffd3191f97dec5e4347ccaf6bdd1e7e8e4dba1d5
payce06 <paycegrossman@gmail.com>
2025-06-22 15:18:18 -0400

    script-js

--- script.js
+++ script.js
@@ -12,3 +12,22 @@ function randomHole() {
     const index = Math.floor(Math.random() * holes.length);
     return holes[index];
 }
+
+function showMole() {
+    const hole = randomHole();
+    console.log("Hole", hole)
+    const mole = hole.querySelector('.mole');
+    mole.classList.add('up');
+
+    mole.onclick = () => {
+        score++;
+        scoreDisplay.textContent = score;
+        mole.classList.remove('up');
+        mole.onclick = null;
+    };
+
+    setTimeout(() =>{
+        mole.classList.remove('up');
+        mole.onclick = null;
+    }, 1000);
+}
